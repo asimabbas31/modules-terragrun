a0533705942c3f82adfcb897e38b07dcfb475d9e
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-08-07 13:03:39 +0200

    Create variables.tf

---
+++ terragrunt/modules/network/variables.tf
@@ -0,0 +1,20 @@
+variable "availability_zones" {
+  type = list
+}
+
+
+variable "cidr_block" {
+  type = string
+  }
+
+variable "env" {
+  type        = string
+}
+
+variable "vpcid" {
+  type        = string
+}
+
+variable "app" {
+  type        = string
+}
