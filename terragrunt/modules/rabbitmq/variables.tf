ff3ee2206fc73905f9c5c30fa7a52d5da842bbb5
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-09-06 13:12:50 +0200

    Create variables.tf

---
+++ terragrunt/modules/rabbitmq/variables.tf
@@ -0,0 +1,16 @@
+variable "env" {
+  type        = string
+}
+
+variable "vpcid" {
+  type        = string
+}
+
+variable "key_name" {
+  type        = string
+}
+
+
+variable "asgsc_application" {
+  type        = string
+}
