8494b194f4052cccad06d46484f2e724d1acc4cf
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-09-09 14:01:29 +0200

    Create variables.tf

---
+++ terragrunt/modules/lunchtemplate/variables.tf
@@ -0,0 +1,37 @@
+variable "instance_type" {
+  description = "API Instances for app"
+}
+
+variable "key_name" {
+  type        = string
+ }
+
+
+variable "env" {
+  type        = string
+}
+
+variable "sgapp" {
+  type        = string
+}
+
+variable "app" {
+  type        = string
+}
+
+
+ data "aws_ami" "api" {
+   most_recent = true
+   filter {
+     name   = "name"
+     values = ["api-latest"]
+   }
+   owners = ["957382640169"]
+ }
+variable "instance_policy" {
+  type        = string
+}
+
+variable "bucket_name" {
+  type        = string
+}
