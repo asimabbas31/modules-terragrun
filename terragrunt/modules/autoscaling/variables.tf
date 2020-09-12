6ba8cc6ba62a46193a7a7a7aecf5d76e0868e80d
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-09-12 23:10:43 +0200

    Create variables.tf

---
+++ terragrunt/modules/autoscaling/variables.tf
@@ -0,0 +1,19 @@
+
+
+variable "asg_aws_subnet_ids" {
+  type = list(string)
+}
+
+
+variable "env" {
+  type        = string
+}
+
+variable "app" {
+  type        = string
+}
+
+
+variable "lt_id" {
+  type        = string
+}
