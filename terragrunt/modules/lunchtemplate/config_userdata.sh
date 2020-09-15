9dd68f2aa051d2be1a0c1beab56687e710325070
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-09-15 23:24:39 +0200

    Update config_userdata.sh

--- terragrunt/modules/lunchtemplate/config_userdata.sh
+++ terragrunt/modules/lunchtemplate/config_userdata.sh
@@ -5,10 +5,11 @@ cd /tmp
 tar xf api.tar.gz
 mv /tmp/api/* /var/www/html/
 
+chown www-data.www-data /var/www/html/storage/ -R
 credstash -r eu-west-1 -t  api_stage_credstash_store getall --format dotenv > /var/www/html/.env
-
 
 systemctl start apache2
 cd /var/www/html/
+
 
 php artisan queue:work redis --queue=async-commands,bonus-payment,cleanup,device-metadata,default,eta,food-delivery-order-pm,high,notification,order-payments,paygate_rmq_callback_q,paygate-topup,payment,place,projections,safeboda-api-queue,sms,transaction-update,upload-merchants&
