a4270a7b60fcec86a282d45cc4e0ba0b471f18e5
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-04 00:08:40 +0200

    Update eks.tf

--- terragrunt/modules/rabbitmq/eks.tf
+++ terragrunt/modules/rabbitmq/eks.tf
@@ -91,12 +91,19 @@ resource "aws_eks_node_group" "node" {
   node_group_name = "rabbitmq-api_${var.env}"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = var.public_subnet
-
+  security_group_id =  "sg-07dda92883c803056"
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
   }
+  
+  tags = {
+    Name = "rabbitmq-api_${var.env}"
+    env = var.env
+    Project = "API"
+  }
+
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
