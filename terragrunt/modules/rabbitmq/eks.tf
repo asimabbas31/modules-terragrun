194d5f73fe4db97de6fe0e26faf901f30595d917
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-03 23:50:43 +0200

    Update eks.tf

--- terragrunt/modules/rabbitmq/eks.tf
+++ terragrunt/modules/rabbitmq/eks.tf
@@ -90,7 +90,7 @@ resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "rabbitmq-api_${var.env}"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = var.asgsc_application
+  subnet_ids      = var.public_subnet
 
   scaling_config {
     desired_size = 1
