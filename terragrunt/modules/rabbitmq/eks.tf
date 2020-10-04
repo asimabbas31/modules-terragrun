4f33414ea4746d01eb502cece701abd13b18cdcd
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-04 12:28:14 +0200

    Update eks.tf

--- terragrunt/modules/rabbitmq/eks.tf
+++ terragrunt/modules/rabbitmq/eks.tf
@@ -98,7 +98,7 @@ resource "aws_eks_node_group" "node" {
   }
   remote_access {
     ec2_ssh_key               = var.key_name
-    source_security_group_ids = var.rmqsg
+    source_security_group_ids = ["sg-07dda92883c803056"]
   }
   
   tags = {
