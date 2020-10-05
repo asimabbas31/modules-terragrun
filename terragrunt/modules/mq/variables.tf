6cf8500526e4611106b26e0f52593b28fe0783a0
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-05 09:28:26 +0200

    Create variables.tf

---
+++ terragrunt/modules/mq/variables.tf
@@ -0,0 +1,20 @@
+variable "instance_type" {
+  description = "API Instances for app"
+}
+
+variable "key_name" {
+  type        = string
+ }
+
+
+variable "env" {
+  type        = string
+}
+
+variable "rmqsg" {
+  type        = string
+}
+
+variable "app" {
+  type        = string
+}
