960315df038c6195c1639493c199273d746805ea
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-05 09:30:02 +0200

    Create config_userdata.sh

---
+++ terragrunt/modules/mq/config_userdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update && sudo apt install wget -y
+apt install apt-transport-https -y
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+echo "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang-22.x" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+apt update
+apt install rabbitmq-server -y
+systemctl is-enabled rabbitmq-server.service
+systemctl enable rabbitmq-server
+rabbitmq-plugins enable rabbitmq_management
+ufw allow proto tcp from any to any port 5672,15672
+rabbitmqctl add_user sb-admin hpAFj5JNvMxz9JuApBUz
