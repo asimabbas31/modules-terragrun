0f36c763968060a81519683f83e698deb9084708
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-06 12:19:05 +0200

    Update config_userdata.sh

--- terragrunt/modules/mq/config_userdata.sh
+++ terragrunt/modules/mq/config_userdata.sh
@@ -14,21 +14,3 @@ ufw allow proto tcp from any to any port 5672,15672
 rabbitmqctl add_user sb-admin hpAFj5JNvMxz9JuApBUz
 rabbitmqctl set_user_tags sb-admin administrator
 rabbitmqctl delete_user guest
-
-
-curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
-
-echo "license_key: 447b2924c3d83133c6aff7e11e0e6571f679a2ec" | sudo tee -a /etc/newrelic-infra.yml
-printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
-
-apt-get update
-
-apt-get install newrelic-infra -y
-
-cat <<EOF > /etc/newrelic-infra/logging.d/test.sh
-logs:
-  - name: legacy-logs
-    file: /var/www/html/storage/logs/*.log
-EOF
-
-systemctl restart newrelic-infra.service
