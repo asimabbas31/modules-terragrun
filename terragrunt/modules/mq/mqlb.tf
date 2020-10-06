12eeecc20d52e997795325ffb2f301ec698d8bed
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-06 11:26:54 +0200

    Update mqlb.tf

--- terragrunt/modules/mq/mqlb.tf
+++ terragrunt/modules/mq/mqlb.tf
@@ -120,6 +120,6 @@ resource "aws_lb_listener" "rabbitmqport" {
 
 
 resource "aws_autoscaling_attachment" "albapple" {
-  autoscaling_group_name = "stage_apple_rmq"
+  autoscaling_group_name = "var.autoscaling_group_apple"
   alb_target_group_arn   = aws_lb_target_group.rabbitmqhttp.arn
 }
