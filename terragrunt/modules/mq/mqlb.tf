b209d06fcbce077bda25f7e0e6faca603adbd1b8
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-06 09:42:23 +0200

    Create mqlb.tf

---
+++ terragrunt/modules/mq/mqlb.tf
@@ -0,0 +1,89 @@
+resource "aws_lb" "mq" {
+  name                             = "rabbitmq-${var.env}"
+  internal                         = false
+  load_balancer_type               = "network"
+  enable_deletion_protection       = var.deletion_protection
+  enable_cross_zone_load_balancing = true
+  ip_address_type                  = "ipv4" # cannot dual stack with specified addresses
+
+  subnets = var.public_subnets
+  tags = {
+    Name        = "rabbitmq"
+    source      = "terraform"
+    project     = "API"
+    env        = var.env
+  }
+}
+
+# docs: https://aws.amazon.com/premiumsupport/knowledge-center/security-group-load-balancer/
+# per docs, the SG is attached to the target instances (or the redis instances, in this case)
+# when using an NLB
+
+resource "aws_security_group" "rabbitmq" {
+  name        = "rabbitmq loadbalancer"
+  description = "rabbitmq ${var.env}"
+  vpc_id      = var.vpcid
+
+  ingress {
+    from_port   = 15672
+    to_port     = 15672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Rabbitmq loadbalancer"
+    source      = "terraform"
+    project     = "API"
+    env          = var.env
+  }
+}
+
+
+# the protocol is technically TLS rather than basic TCP, however we go from
+# internet --> LB --> TLS, so this will simply forward dumb TCP.
+
+resource "aws_lb_target_group" "rabbitmqhttp" {
+  protocol    = "TCP"
+  port        = 15672
+  target_type = "instance"
+  vpc_id      = var.vpcid
+  tags = {
+    Name        = "rabbitmq"
+    source      = "terraform"
+    project     = "API"
+    env = var.env
+  }
+  health_check {
+    enabled             = true
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 10
+  }
+}
+
+
+
+resource "aws_lb_listener" "rabbitmqhttp" {
+  load_balancer_arn = aws_lb.rabbitmq.arn
+  protocol          = "TCP"
+  port              = 15672
+  default_action {
+    target_group_arn = aws_lb_target_group.rabbitmqhttp.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "rabbitmq" {
+  target_group_arn = aws_lb_target_group.rabbitmqhttp.arn
+  
+}
