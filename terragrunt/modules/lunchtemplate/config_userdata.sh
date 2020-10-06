af72d9b8bb41ef6194d68d54ccb3dcf49583e269
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-06 12:19:42 +0200

    Update config_userdata.sh

--- terragrunt/modules/lunchtemplate/config_userdata.sh
+++ terragrunt/modules/lunchtemplate/config_userdata.sh
@@ -24,3 +24,20 @@ php artisan queue:work interop --queue=sb-api-integration --tries=1&
 
 
 php artisan queue:work redis --queue=async-commands,bonus-payment,cleanup,device-metadata,default,eta,food-delivery-order-pm,high,notification,order-payments,paygate_rmq_callback_q,paygate-topup,payment,place,projections,safeboda-api-queue,sms,transaction-update,upload-merchants&
+
+curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+
+echo "license_key: 447b2924c3d83133c6aff7e11e0e6571f679a2ec" | sudo tee -a /etc/newrelic-infra.yml
+printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+
+apt-get update
+
+apt-get install newrelic-infra -y
+
+cat <<EOF > /etc/newrelic-infra/logging.d/test.sh
+logs:
+  - name: legacy-logs
+    file: /var/www/html/storage/logs/*.log
+EOF
+
+systemctl restart newrelic-infra.service
