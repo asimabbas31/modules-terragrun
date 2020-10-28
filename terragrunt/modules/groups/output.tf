74eee21d7c1a18c0e182f7b6b52595790ba4d9ec
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-10-28 08:24:39 +0100

    Update output.tf

--- terragrunt/modules/groups/output.tf
+++ terragrunt/modules/groups/output.tf
@@ -1,9 +1,7 @@
-
+output "sgapp" {
+  value       = aws_security_group.sgapp.id
+}
 
 output "lbsc" {
   value       = aws_security_group.loadbalancer_app.id
-}
-
-output "rmqsg" {
-  value       = aws_security_group.rmq.id
 }
