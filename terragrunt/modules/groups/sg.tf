02a7e6204b8d63856491e982c5a3528c2923cd48
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-11-03 23:39:06 +0100

    Update sg.tf

--- terragrunt/modules/groups/sg.tf
+++ terragrunt/modules/groups/sg.tf
@@ -137,6 +137,17 @@ resource "aws_security_group" "rmqsg" {
     
   }
   
+     ingress {
+    description      = ""
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  
+  
+  
 
   # Allow all outbound requests
 egress {
