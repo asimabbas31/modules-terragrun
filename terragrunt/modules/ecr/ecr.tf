8f103f8cfae8d9abb6e7c59f6c80205297dc9fdd
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-11-22 17:06:12 +0100

    Update ecr.tf

--- terragrunt/modules/ecr/ecr.tf
+++ terragrunt/modules/ecr/ecr.tf
@@ -1,3 +1,13 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+terraform {
+  # Intentionally empty. Will be filled by Terragrunt.
+  backend "s3" {}
+}
+
 resource "aws_ecr_repository" "asim" {
   name                 = ${app}_${var.env}"
   image_tag_mutability = "MUTABLE"
