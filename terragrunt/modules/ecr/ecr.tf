08020f05022c1c786e1f55599eb881b09a4d1f77
Asim Abbas <46396661+asimabbas31@users.noreply.github.com>
2020-11-22 17:09:35 +0100

    Update ecr.tf

--- terragrunt/modules/ecr/ecr.tf
+++ terragrunt/modules/ecr/ecr.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "aws_ecr_repository" "asim" {
-  name                 = ${app}_${var.env}"
+  name                 = ${app}_${var.env}
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
